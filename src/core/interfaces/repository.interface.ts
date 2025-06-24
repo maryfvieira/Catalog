e131b234d63c545c695f93172125119642dcaf77
Mary Felvie <mary.feliciano@gmail.com>
2025-06-24 14:30:09 -0300

    correcoes

--- src/core/interfaces/repository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { IEntity } from "../../interfaces/entity.interface";
-
-export interface IRepository<T extends IEntity> {
-  create(entity: Partial<T>): Promise<T>;
-  update(id: string | number, entity: Partial<T>): Promise<T | null>;
-  delete(id: string | number): Promise<boolean>;
-  findById(id: string | number): Promise<T | null>;
-  findAll(): Promise<T[]>;
-}
--- src/core/unitofWork/transaction.ts
+++
@@ -1,3 +0,0 @@
-export class Transaction {
-    constructor(public commit: () => Promise<void>, public rollback: () => Promise<void>) {}
-  }
